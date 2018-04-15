b9671de3c32bf9847e6d066d9b05a2c7dffa7e51
swagata <swagatadas.wb@gmail.com>
2018-04-15 13:32:22 +0530

    init commit

---
+++ app/src/main/java/com/example/arghyaghosh/samplemap/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.arghyaghosh.samplemap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng kolkata = new LatLng(22.63, 88.83);
+        googleMap.addMarker(new MarkerOptions().position(kolkata)
+                .title("Marker in Kolkata"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(kolkata));
+    }
+}
